4f8233573a8abb06a2bf981c634f31954d0366c1
MadScientist <1xlr8ghast@gmail.com>
2026-02-24 13:03:51 -0500

    Initial Structure

---
+++ 2026 Robot Code/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,31 @@
+package frc.robot;
+
+public final class Constants {
+    public static class Shooter{
+
+    }
+
+    public static class Climber{
+
+    }
+
+    public static class Intake{
+
+    }
+
+    public static class Feeder{
+
+    }
+
+    public static class Swerve{
+
+    }
+
+    public static class Limelight{
+
+    }
+
+    public static class Quest{
+        
+    }
+}
